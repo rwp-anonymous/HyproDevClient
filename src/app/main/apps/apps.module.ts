9862b390509b7b9d7f4f420fce3e4b61193ff768
Chamod Nimz <chamodnimsaradx@gmail.com>
2019-06-28 01:12:05 +0530

    initial commit

---
+++ src/app/main/apps/apps.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import { FuseSharedModule } from '@fuse/shared.module';
+
+const routes = [
+    {
+        path        : 'dashboards/analytics',
+        loadChildren: './dashboards/analytics/analytics.module#AnalyticsDashboardModule'
+    },
+    {
+        path        : 'calendar',
+        loadChildren: './calendar/calendar.module#CalendarModule'
+    },
+    {
+        path        : 'e-commerce',
+        loadChildren: './e-commerce/e-commerce.module#EcommerceModule'
+    }
+];
+
+@NgModule({
+    imports     : [
+        RouterModule.forChild(routes),
+        FuseSharedModule
+    ]
+})
+export class AppsModule
+{
+}
---
+++ src/app/navigation/navigation.ts
@@ -0,0 +1,74 @@
+import { FuseNavigation } from '@fuse/types';
+
+export const navigation: FuseNavigation[] = [
+    {
+        id       : 'applications',
+        title    : 'Applications',
+        translate: 'NAV.APPLICATIONS',
+        type     : 'group',
+        icon     : 'apps',
+        children : [
+            {
+                id       : 'dashboards',
+                title    : 'Dashboards',
+                translate: 'NAV.DASHBOARDS',
+                type     : 'collapsable',
+                icon     : 'dashboard',
+                children : [
+                    {
+                        id   : 'analytics',
+                        title: 'Analytics',
+                        type : 'item',
+                        url  : '/apps/dashboards/analytics'
+                    }
+                ]
+            },
+            {
+                id       : 'calendar',
+                title    : 'Calendar',
+                translate: 'NAV.CALENDAR',
+                type     : 'item',
+                icon     : 'today',
+                url      : '/apps/calendar'
+            },
+            {
+                id       : 'e-commerce',
+                title    : 'E-Commerce',
+                translate: 'NAV.ECOMMERCE',
+                type     : 'collapsable',
+                icon     : 'shopping_cart',
+                children : [
+                    {
+                        id        : 'products',
+                        title     : 'Products',
+                        type      : 'item',
+                        url       : '/apps/e-commerce/products',
+                        exactMatch: true
+                    },
+                    {
+                        id        : 'productDetail',
+                        title     : 'Product Detail',
+                        type      : 'item',
+                        url       : '/apps/e-commerce/products/1/printed-dress',
+                        exactMatch: true
+                    },
+                    {
+                        id        : 'orders',
+                        title     : 'Orders',
+                        type      : 'item',
+                        url       : '/apps/e-commerce/orders',
+                        exactMatch: true
+                    },
+                    {
+                        id        : 'orderDetail',
+                        title     : 'Order Detail',
+                        type      : 'item',
+                        url       : '/apps/e-commerce/orders/1',
+                        exactMatch: true
+                    }
+                ]
+            }
+            
+        ]
+    }
+];
